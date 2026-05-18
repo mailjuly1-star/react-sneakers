3d041a1740cfeed968acb4680dff1ed3304936c4
mailjuly1-star <mailjuly1@gmail.com>
2026-05-18 13:48:11 +0300

    Create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Card from "./components/Card";
+import Drawer from "./components/Drawer";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Drawer />
+      <Header />
+      <div className="content">
+        <div className="content__top">
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img width={14} height={14} src="img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
+        <ul className="cards">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </ul>
+      </div>
     </div>
   );
 }
